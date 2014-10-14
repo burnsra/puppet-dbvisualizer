1cc0300cba4d9d8916b241f26314af84094f5444
Robert Burns <burnsra@gmail.com>
2014-10-14 05:29:57 -0400

    Cleanup spec

--- spec/classes/dbvisualizer_spec.rb
+++ spec/classes/dbvisualizer_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'dbvisualizer' do
   it do
-    should contain_package('DbVisualizer').with({
+    should contain_package('dbvisualizer').with({
       :flavor   => 'tgz',
       :source   => 'http://www.dbvis.com/product_download/dbvis-9.1.10/media/dbvis_macos_9_1_10.tgz',
       :provider => 'compressed_app'
