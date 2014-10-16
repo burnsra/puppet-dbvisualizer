92d251072ac82bed0e2e1842aaf04455e8a5f008
Robert Burns <burnsra@gmail.com>
2014-10-16 12:19:59 -0400

    Update to version 9.1.11

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,6 @@ class dbvisualizer {
   package { 'dbvisualizer':
     provider => 'compressed_app',
     flavor   => 'tgz',
-    source   => 'http://www.dbvis.com/product_download/dbvis-9.1.10/media/dbvis_macos_9_1_10.tgz'
+    source   => 'http://www.dbvis.com/product_download/dbvis-9.1.11/media/dbvis_macos_9_1_11.tgz'
   }
 }
