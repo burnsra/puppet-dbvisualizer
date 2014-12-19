976a8e00b83acf3a3bb45ee9993a6a55244d0466
Robert Burns <burnsra@gmail.com>
2014-12-19 12:20:41 -0500

    Update to version 9.1.12

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,6 @@ class dbvisualizer {
   package { 'dbvisualizer':
     provider => 'compressed_app',
     flavor   => 'tgz',
-    source   => 'http://www.dbvis.com/product_download/dbvis-9.1.11/media/dbvis_macos_9_1_11.tgz'
+    source   => 'http://www.dbvis.com/product_download/dbvis-9.1.12/media/dbvis_macos_9_1_12.tgz'
   }
 }
--- spec/classes/dbvisualizer_spec.rb
+++ spec/classes/dbvisualizer_spec.rb
@@ -4,7 +4,7 @@ describe 'dbvisualizer' do
   it do
     should contain_package('dbvisualizer').with({
       :flavor   => 'tgz',
-      :source   => 'http://www.dbvis.com/product_download/dbvis-9.1.11/media/dbvis_macos_9_1_11.tgz',
+      :source   => 'http://www.dbvis.com/product_download/dbvis-9.1.12/media/dbvis_macos_9_1_12.tgz',
       :provider => 'compressed_app'
     })
   end
